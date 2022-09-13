c74bf82330653accba9e88dc82d27dea0f3ed003
guscarpim <gustavoscarpim@gmail.com>
2022-09-12 22:16:32 -0300

    Projeto inicial

---
+++ server/src/server.js
@@ -0,0 +1,19 @@
+import express, { application } from 'express'
+
+const app = express()
+
+app.get('/ads', (req, res) => {
+  return res.json([
+    {
+      id: 1, nome: 'Anuncio 1'
+    },
+    {
+      id: 2, nome: 'Anuncio 2'
+    },
+    {
+      id: 3, nome: 'Anuncio 3'
+    }
+  ])
+})
+
+app.listen(3333)
